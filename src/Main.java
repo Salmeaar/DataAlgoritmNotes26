e78c04d8228e629fdd271e5e095fe5f856579526
salmeaar <salmeaar@arcada.fi>
2026-01-14 15:32:28 +0200

    Lektion 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello");
+
+        Car car = new Car("Mazda");
+        Car electricvehicle = new Car("Mitsubishi");
+
+        System.out.println(car.getName());
+        car.setName("Toyota");
+        System.out.println(car.getName());
+        System.out.println(electricvehicle.getName());
+
+
+
     }
 }

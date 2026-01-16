1dab51ccb03087c5ac7f4ad122ac22072b54f06b
salmeaar <salmeaar@arcada.fi>
2026-01-16 10:46:54 +0200

    lektion 2 cc done

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello");
 
-        Shirt tish = new Shirt("Polo","Röd","20");
+        Shirt tish = new Shirt(" Polo "," Röd "," 20 ");
+        Shirt långarm = new Shirt(" College ", " Beige ", " 50 ");
+
+        System.out.println("Skjorta 1:" +  tish.getColor() + tish.getBrand() + "pris:" + tish.getPrice());
+        System.out.println("Skjorta 2:" + långarm.getColor() + långarm.getBrand() + "pris:" + långarm.getPrice());
+
+
     }
 }
--- src/Shirt.java
+++ src/Shirt.java
@@ -1,3 +1,7 @@
 public class Shirt extends Clothing{
 
+
+    public Shirt(String brand, String color, String price){
+        super(brand, color, price);
+    }
 }
